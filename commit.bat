8dc005b58192a6bb7c31e49a49e08205c07248a9
hahahaHan233 <1254686887@qq.com>
2024-04-28 04:43:24 +1000

    wh - 2024-04-28  4:43:24.41

---
+++ commit.bat
@@ -0,0 +1,15 @@
+@echo off
+set /p commit_msg="Enter your commit message: "
+set datetime=%date% %time%
+
+:: Add all changed files to the staging area
+git add .
+
+:: Commit the changes with a timestamp
+git commit -m "%commit_msg% - %datetime%"
+
+:: Push the changes to the remote repository
+git push origin master
+
+echo Commit and push completed.
+pause
