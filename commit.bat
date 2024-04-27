221df6d6d503172dd1de2976e2e0a542e0bc1117
hahahaHan233 <1254686887@qq.com>
2024-04-28 06:35:41 +1000

    1 - 2024-04-28  6:35:41.62

--- commit.bat
+++ commit.bat
@@ -1,3 +1,5 @@
+cd /d "D:\Han\2023_ANU\2024S1\COMP8535 - Engineering Data Analytics\Homework\Group\CF_ML100k"
+
 @echo off
 set /p commit_msg="Enter your commit message: "
 set datetime=%date% %time%
